74949f17f4a36da1fd98d521ceffb36775508ade
Paolo <paolo.bets@gmail.com>
2026-04-19 14:56:47 +0200

    chore: bump version to 0.0.5

--- hiris/app/server.py
+++ hiris/app/server.py
@@ -88,4 +88,4 @@ async def _serve_config(request: web.Request) -> web.Response:
 
 
 async def _handle_health(request: web.Request) -> web.Response:
-    return web.json_response({"status": "ok", "version": "0.0.4"})
+    return web.json_response({"status": "ok", "version": "0.0.5"})
